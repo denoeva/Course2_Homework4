6aeb736fd921b19fca7cc8e74cdeba3f4075a814
elena.denoeva <denoeva.elena@skyeng.ru>
2023-04-22 10:14:19 +0300

    Initial commit

---
+++ src/test/java/pro/sky/homeworkmavenspringboot/HomeworkMavenSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkmavenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkMavenSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
